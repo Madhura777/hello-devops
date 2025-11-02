f5a839c078d727542f4d599c3f3b58b3cf509dc9
Madhura777 <madhuraaher1995@gmail.com>
2025-11-02 09:49:40 +0530

    Updated login feature for demonstration

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 def hello():
     return "Hello DevOps"
 # Login feature added
-
+# Added a login route simulation for Task 2 demonstration
+print("Login feature active")
 
 if __name__ =="__main__":
     app.run(host="0.0.0.0",port=5000)
